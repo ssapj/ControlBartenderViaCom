1cfd5e0e9efb3cf7136dd5bb570f46a02dc3e4f7
ssapj <toyama@seagullscientific.com>
2015-04-30 11:55:20 +0900

    add a sample

---
+++ Sample/BtMoveAllObjects/ActualControl.cs
@@ -0,0 +1,57 @@
+﻿using BarTender;
+using Ssapj.ControlBartenderViaCom;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace BtMoveAllObjects
+{
+    class ActualControl : ControlBartenderBase
+    {
+        private double _x = 1.5;
+        private double _y = 1.5;
+        private BtUnits _btUnits = BtUnits.btUnitsCentimeters;
+
+        public void ControlBtwFile(string btwfilepath)
+        {
+            try
+            {
+                using (var hProcess = Process.GetProcessById(this.BartenderProcessId))
+                {
+                }
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Restart BarTender");
+                this.RestartBartender();
+            }
+
+            var btFormat = this.BartenderApplication.Formats.Open(btwfilepath);
+
+            btFormat.MeasurementUnits = this._btUnits;
+
+            var targetDesignObjects = Enumerable.Range(1, btFormat.Objects.Count)
+                .Select(i => btFormat.Objects.Item(i))
+                .Where(xs => xs.Type != BarTender.BtObjectType.btObjectError
+                    && xs.Type != BarTender.BtObjectType.btObjectRFID
+                    && xs.Type != BarTender.BtObjectType.btObjectMagneticStripe
+                    && xs.Type != BarTender.BtObjectType.btObjectShape)
+                    ;
+
+            foreach (var item in targetDesignObjects)
+            {
+                item.X += this._x;
+                item.Y += this._y;
+            }
+
+            btFormat.Close(BarTender.BtSaveOptions.btSaveChanges);
+
+            Marshal.FinalReleaseComObject(btFormat);
+            btFormat = null;
+        }
+
+    }
+
+
+}
---
+++ Sample/BtMoveAllObjects/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace BtMoveAllObjects
+{
+    class Program
+    {
+        [DllImport("Kernel32")]
+        static extern bool SetConsoleCtrlHandler(HandlerRoutine Handler, bool Add);
+        delegate bool HandlerRoutine(CtrlTypes CtrlType);
+
+        private ActualControl _ctrlBT;
+
+        private enum CtrlTypes
+        {
+            CTRL_C_EVENT = 0,
+            CTRL_BREAK_EVENT = 1,
+            CTRL_CLOSE_EVENT = 2,
+            CTRL_LOGOFF_EVENT = 5,
+            CTRL_SHUTDOWN_EVENT = 6
+        }
+
+
+        // ハンドラ・ルーチン
+        bool myHandler(CtrlTypes ctrlType)
+        {
+            this._ctrlBT.Dispose();
+            return false;
+        }
+
+
+        Program()
+        {
+            SetConsoleCtrlHandler(myHandler, true);
+            this._ctrlBT = new ActualControl();
+        }
+
+
+        void Run(string[] args)
+        {
+            try
+            {
+                var btInstanceTask = this._ctrlBT.StartBartenderAsync();
+
+                btInstanceTask.Wait();
+
+                foreach (string btwfilepath in args)
+                {
+                    Console.WriteLine("Begin : {0}", btwfilepath);
+
+                    try
+                    {
+                        this._ctrlBT.ControlBtwFile(btwfilepath);
+                        Console.WriteLine("Success : {0}", btwfilepath);
+                    }
+                    catch (COMException come)
+                    {
+                        Console.WriteLine("Fail : {0}\n{1}", btwfilepath, come.Message);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Fail : {0}\n{1}", btwfilepath, e.Message);
+                    }
+
+                }
+
+            }
+            catch (COMException come)
+            {
+                Console.WriteLine(come.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                this._ctrlBT.Dispose();
+            }
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            (new Program()).Run(args);
+        }
+
+    }
+
+}
+
